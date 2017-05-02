3ccbfa19e928c209a5831ce0443a62d31852eaa1
Prithu Goswami <prithugoswami524@gmail.com>
2017-05-02 20:15:55 +0530

    Added few comments

--- amt.py
+++ amt.py
@@ -1,4 +1,6 @@
 '''
+This script might seem a little messy and ugly and I know maybe there is better and effecient way to do some of the tasks
+but I am unaware of them at the moment and am a begginer in Python and this is my first, or maybe second python script.
 
 TO-DO
 1. Add a way to notify when the script is done running
